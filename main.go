21e22b716c85234f2abd70e13b047bda8ce01420
Diogenis <55108282+wisepythagoras@users.noreply.github.com>
2023-01-08 13:47:21 -0500

    Read from and write to serial device

---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"sync"
+
+	"go.bug.st/serial"
+)
+
+var wg *sync.WaitGroup
+
+func read(portPtr *serial.Port) {
+	defer wg.Done()
+
+	port := *portPtr
+
+	for {
+		buff := make([]byte, 256)
+		n, err := port.Read(buff)
+
+		if err != nil {
+			fmt.Println("Read Error:", err)
+			continue
+		}
+
+		fmt.Println(n, string(buff))
+
+		if n == 0 {
+			fmt.Println("EOF")
+		}
+	}
+}
+
+func main() {
+	device := flag.String("device", "", "The serial device to open")
+	flag.Parse()
+
+	if len(*device) == 0 {
+		log.Fatal("No serial device was selected")
+	}
+
+	ports, err := serial.GetPortsList()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(ports) == 0 {
+		log.Fatal("No serial ports found")
+	}
+
+	found := false
+
+	for _, port := range ports {
+		fmt.Println("Found port", port)
+
+		if port == *device {
+			found = true
+		}
+	}
+
+	if !found {
+		log.Fatalf("No such serial device: %q\n", *device)
+	}
+
+	mode := &serial.Mode{
+		BaudRate: 9600,
+	}
+	port, err := serial.Open(*device, mode)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	msg := []byte("Test message")
+	msg = append(msg, 0)
+	port.Write(msg)
+
+	wg = new(sync.WaitGroup)
+	wg.Add(1)
+
+	go read(&port)
+
+	wg.Wait()
+}
